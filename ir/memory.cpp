741aa757e396c52ae38b6dd68c7fa9e1faa5558f
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2024-11-29 15:45:11 +0000

    byte type performs raw memory loads

--- ir/memory.cpp
+++ ir/memory.cpp
@@ -586,6 +586,18 @@ vector<Byte> Memory::valueToBytes(const StateValue &val, const Type &fromType,
 
     for (unsigned i = 0; i < bytesize; ++i)
       bytes.emplace_back(*this, StateValue(expr(p()), expr(val.non_poison)), i);
+  } else if (fromType.isByteType()) {
+    unsigned bitsize = val.bits();
+    unsigned bytesize = divide_up(bitsize, Byte::bitsByte());
+
+    StateValue byval = fromType.toInt(s, val);
+    pad(byval, bytesize * Byte::bitsByte() - bitsize, s);
+
+    for (unsigned i = 0; i < bytesize; ++i)
+      bytes.emplace_back(
+        *this,
+        byval.value.extract((i + 1) * Byte::bitsByte() - 1, i * Byte::bitsByte())
+      );
   } else {
     assert(!fromType.isAggregateType() || isNonPtrVector(fromType));
     StateValue bvval = fromType.toInt(s, val);
@@ -669,7 +681,17 @@ StateValue Memory::bytesToValue(const vector<Byte> &bytes, const Type &toType) {
       loaded_ptr = expr::mkIf(is_ptr, loaded_ptr, Pointer::mkNullPointer(*this)());
     }
     return { std::move(loaded_ptr), std::move(non_poison) };
+  } else if (toType.isByteType()) {
+    auto bitsize = toType.bits();
+    assert(divide_up(bitsize, Byte::bitsByte()) == bytes.size());
 
+    StateValue val;
+    for (unsigned i = 0, e = bytes.size(); i < e; ++i) {
+      auto &b = bytes[i];
+      StateValue v(expr(b.p), !b.isPoison());
+      val = i == 0 ? std::move(v) : v.concat(val);
+    }
+    return val;
   } else {
     assert(!toType.isAggregateType() || isNonPtrVector(toType));
     auto bitsize = toType.bits();
@@ -2153,6 +2175,9 @@ unsigned Memory::getStoreByteSize(const Type &ty) {
 
   if (ty.isPtrType())
     return divide_up(bits_program_pointer, 8);
+
+  if (ty.isByteType())
+    return divide_up(ty.bits(), Byte::bitsByte());
 
   auto aty = ty.getAsAggregateType();
   if (aty && !isNonPtrVector(ty)) {
